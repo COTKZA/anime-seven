72886b79609f2a71a7f458a68f6bd8e101ebd610
Jirasak Suktakua <49307492+COTKZA@users.noreply.github.com>
2025-09-12 16:53:10 +0700

    update folder admin

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
-const AuthController = require('../controllers/auth/AuthController')
+const AuthController = require('../controllers/admin/auth/AuthController')
 
 router.post('/auth/register', AuthController.register)
 
